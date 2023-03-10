c341bfe1900b8c7637a57031d45fb97a6db9cbe9
Tiago <tiago@wvtodoz.com.br>
2023-03-10 14:54:46 -0300

    operações em variáveis js

---
+++ JavaScript/assets/js/script.js
@@ -0,0 +1,25 @@
+// let nome = "Tiago Martins de Almeida" // String
+// let idade = 37 // Number
+// let logado = true // Boolean 
+// let lista = ["laranja", "maçã", "uva"] // Array
+// let nomeCompleto = {nome: "Tiagão", idade: 37} // Objeto
+
+// // o comando abaixo exibe os dados no CONSOLE do navegador
+// console.log(typeof nomeCompleto)
+
+
+// let carro = "Ferrari"
+// let priceBolo = 19.99 
+// let cidade = "São Paulo"
+
+// console.log(carro)
+// console.log(priceBolo)
+// console.log(cidade)
+
+let primeiroNome = "Tiago";
+let nomeDoMeio = "Martins";
+let ultimoNome = "de Almeida";
+
+let nomeCompleto = primeiroNome + " " + nomeDoMeio + " " + ultimoNome;
+
+console.log(nomeCompleto);
